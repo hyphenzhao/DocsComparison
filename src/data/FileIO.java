56ff6370506faac9e8994a22de09e32d0481ff61
Haifeng Zhao <hzha7521@uni.sydney.edu.au>
2018-12-14 16:41:43 +1100

    Now could support .docx and .doc files

--- src/data/FileIO.java
+++ src/data/FileIO.java
@@ -7,6 +7,12 @@ import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.poi.hwpf.HWPFDocument;
+import org.apache.poi.hwpf.extractor.WordExtractor;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 
 public class FileIO {
 	private String filename;
@@ -19,24 +25,50 @@ public class FileIO {
 	
 	public FileIO(File file) {
 		this.file = file;
+		this.filename = this.file.getName();
 	}
 	
 	public ArrayList<String> loadFileText() {
 		try {
 			if (file.isFile() && file.exists())
 	        {
-	            InputStreamReader inputStream = new InputStreamReader(
-	                    new FileInputStream(file));
-	            BufferedReader bufferedReader = new BufferedReader(inputStream);
-	            String lineText = null;
+				//System.out.println(this.filename);
+				if(this.filename.toLowerCase().endsWith("doc")) {
+		            FileInputStream fis = new FileInputStream(this.file.getAbsolutePath());
+		            HWPFDocument document = new HWPFDocument(fis);
+		            WordExtractor extractor = new WordExtractor(document);
+		            String[] fileData = extractor.getParagraphText();
+		            for (int i = 0; i < fileData.length; i++)
+		            {
+		                if (fileData[i] != null)
+		                	rawTextLines.add(fileData[i]);
+		            }
+		            document.close();
+		            fis.close();
+				} else if(this.filename.toLowerCase().endsWith("docx")) { 
+					FileInputStream fis = new FileInputStream(file.getAbsolutePath());
+	                XWPFDocument document = new XWPFDocument(fis);
+	                List<XWPFParagraph> paragraphs = document.getParagraphs();
 
-	            while ((lineText = bufferedReader.readLine()) != null)
-	            {
-	            	if(lineText != "")
-	            		rawTextLines.add(lineText);
-	            }
-	            bufferedReader.close();
-	            inputStream.close();
+	                for (XWPFParagraph para : paragraphs) {
+	                	rawTextLines.add(para.getText());
+	                }
+	                document.close();
+	                fis.close();
+				} else {
+		            InputStreamReader inputStream = new InputStreamReader(
+		                    new FileInputStream(file));
+		            BufferedReader bufferedReader = new BufferedReader(inputStream);
+		            String lineText = null;
+	
+		            while ((lineText = bufferedReader.readLine()) != null)
+		            {
+		            	if(lineText != "")
+		            		rawTextLines.add(lineText);
+		            }
+		            bufferedReader.close();
+		            inputStream.close();
+				}
 	        }
 		}catch(Exception e){
 			System.out.println("Input file error.");
